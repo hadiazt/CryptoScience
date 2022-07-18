c06902acfc80071f4b7a69b0a849d46d9898d3f3
unknown <hadiazari30@gmail.com>
2022-07-18 15:26:06 +0430

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js")
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_VOICE_STATES] })
+
+const { Token } = require("./data/config.json")
+client.login(Token)
+
