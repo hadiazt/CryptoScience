dbe089d700d7890edf9a086c0c5ff958f8408d3c
unknown <hadiazari30@gmail.com>
2022-07-18 20:23:10 +0430

    Update Info.Module.js

--- src/Functions/Ready/Info.Module.js
+++ src/Functions/Ready/Info.Module.js
@@ -7,15 +7,25 @@ var config = {
     url: Endpoints.Top10Info,
     headers: {}
 };
-
 module.exports = (client) => {
     axios(config).then(response => {
         const msg = new MessageEmbed()
 
         response.data.coins.forEach(coin => {
             msg.setAuthor({ name: coin.name, iconURL: coin.icon, url: coin.websiteUrl })
-            msg.setDescription('\n\n' + '**🏅 Rank : ' + coin.rank + '**\n'+'test')
             msg.setTitle(coin.name + ' Information')
+            msg.setDescription(`
+**<:medal:998614813578117120> Rank : ${coin.rank}**
+
+**<:dollar:998616045856231495> Price : ${coin.price}$**
+
+**<:btc:998615614321070110> PriceBTC : ${coin.priceBtc}**
+
+**Volume : **
+
+**<:web:998617971272454144> [WebSite](${coin.websiteUrl})**
+`)
+
 
             client.channels.cache.get('998534356928843911').send({ embeds: [msg] })
             // console.log(coin);
@@ -26,9 +36,9 @@ module.exports = (client) => {
 
     // icon //
     // name //
-    // rank
-    // price : $
-    // priceBtc
+    // rank //
+    // price : $ //
+    // priceBtc //
     // volume
     // marketCap
     // availableSupply
