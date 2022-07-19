6a2f3541fae13dbe51369155bb62b733f54ca7e0
unknown <hadiazari30@gmail.com>
2022-07-19 17:41:58 +0430

    Create Market.Module.js

---
+++ src/Functions/Ready/Market.Module.js
@@ -0,0 +1,18 @@
+var axios = require('axios');
+
+var config = {
+    method: 'get',
+    url: 'https://api.coinstats.app/public/v1/markets?coinId=bitcoin',
+    headers: {}
+};
+
+axios(config)
+    .then(function (response) {
+        Array.from(response.data, (element, index) => {
+            if (index < 10) {
+                console.log(element);
+            } else return;
+        })
+    }).catch(error => {
+        client.channels.cache.get(errchid).send('```log\n' + error + '\n```')
+    });
