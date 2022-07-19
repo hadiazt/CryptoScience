e5102be76c3a2419f648f09afd6036d16b1af41d
unknown <hadiazari30@gmail.com>
2022-07-19 17:59:17 +0430

    Update Market.Module.js

--- src/Functions/Ready/Market.Module.js
+++ src/Functions/Ready/Market.Module.js
@@ -1,5 +1,7 @@
-const { Endpoints } = require('../../../data/APIEndPoints')
 var axios = require('axios');
+const { MessageEmbed } = require('discord.js');
+const { Endpoints } = require('../../../data/APIEndPoints');
+const { msgchid, errchid } = require('../../../data/config.json')
 
 var config = {
     method: 'get',
@@ -9,11 +11,28 @@ var config = {
 
 
 module.exports = (client) => {
-
     axios(config).then(function (response) {
+        const EMBED = new MessageEmbed()
         Array.from(response.data, (element, index) => {
+            EMBED.setTitle('Last 20 Crypto Trades')
+            EMBED.setDescription(`
+
+**<:medal:998614813578117120> Rank : ${index}**
+
+**<:dollar:998616045856231495> Price : ${element.price}$**
+
+**<:web:998617971272454144> Exchange Site : [${element.exchange}](https://www.google.com/search?q=${element.exchange})**
+
+**<:pair:998943648244834336> Pair : ${element.pair}**
+
+**<:dollar:998616045856231495> Pair Price : ${element.price}$**
+
+**<:count:998619072315019324> Volume : ${element.volume}**
+`)
+            EMBED.setTimestamp()
+            
             if (index < 20) {
-                console.log(element);
+                client.channels.cache.get(msgchid).send({ embeds: [EMBED] })
             } else return;
         })
     }).catch(error => {
