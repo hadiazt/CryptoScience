e396d5c76bed1274cde4ff3aa589bb62438c3e5c
unknown <hadiazari30@gmail.com>
2022-07-19 17:45:46 +0430

    Update APIEndPoints.js

--- data/APIEndPoints.js
+++ data/APIEndPoints.js
@@ -1,4 +1,6 @@
 exports.BaseURL = "https://api.coinstats.app/public/v1";
+
 exports.Endpoints = {
     Top10Info: this.BaseURL + "/coins?skip=0&limit=10",
+    Market: this.BaseURL + "/markets?coinId=bitcoin"
 }
