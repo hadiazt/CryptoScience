fbbbfcca883b608486d1862735b3ff6b4d175da6
unknown <hadiazari30@gmail.com>
2022-07-20 14:50:43 +0430

    Update handler.js

--- src/Functions/handler.js
+++ src/Functions/handler.js
@@ -1,3 +1,4 @@
 exports.CryptoPrice = require('./Ready/CryptoPrice.Module')
 exports.Trades = require('./Ready/Trades.Module')
 exports.News = require('./Ready/News.Module')
+exports.WhaleTrades = require('./Ready/WhaleTrades.Module')
