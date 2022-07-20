88ba604445d2a18844ffcdd76d19d56ca46385f8
unknown <hadiazari30@gmail.com>
2022-07-20 15:26:56 +0430

    Update APIEndPoints.js

--- data/APIEndPoints.js
+++ data/APIEndPoints.js
@@ -1,7 +1,11 @@
+const { APIKEY } = require('./config.json')
+
 exports.BaseURL = "https://api.coinstats.app/public/v1";
+exports.BaseURL2 = "https://api.whale-alert.io/v1";
 
 exports.Endpoints = {
     Info: this.BaseURL + "/coins?skip=0&limit=10",
     Market: this.BaseURL + "/markets?coinId=bitcoin",
-    News: this.BaseURL + "/news?skip=0&limit=10"
+    News: this.BaseURL + "/news?skip=0&limit=10",
+    WhaleTrades: this.BaseURL2 + `/transactions?api_key=${APIKEY}&limit=50`
 }
