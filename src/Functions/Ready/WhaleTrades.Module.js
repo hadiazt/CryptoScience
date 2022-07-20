cc40f7998c67ab2be9c7807074915e0ace49de0a
unknown <hadiazari30@gmail.com>
2022-07-20 15:48:29 +0430

    Update WhaleTrades.Module.js

--- src/Functions/Ready/WhaleTrades.Module.js
+++ src/Functions/Ready/WhaleTrades.Module.js
@@ -5,9 +5,6 @@ const { Channels } = require('../../../data/config.json')
 
 
 module.exports = (client) => {
-    var OneHAgoTS = new Date().getTime() - (1000 * 20 * 20);
-
-    var OneHAgoLS = new Date(OneHAgoTS).toLocaleTimeString();
 
     var config = {
         method: 'get',
@@ -16,9 +13,29 @@ module.exports = (client) => {
     };
 
     axios(config).then(response => {
-        console.log(response.data);
-        
+        const EMBED = new MessageEmbed()
+
+        Array.from(response.data.transactions, (element, index) => {
+            Trades = element
+
+            if (!Trades.from.owner) FO = 'unknown'; else FO = Trades.from.owner;
+            if (!Trades.to.owner) TO = 'unknown'; else TO = Trades.to.owner;
+
+            EMBED.setTitle('Last 30 Whales Crypto Trades')
+            EMBED.setColor('#e83c70')
+            EMBED.setThumbnail(client.user.displayAvatarURL({ size: 2048 }))
+            EMBED.setDescription(`
+
+**<:medal:998614813578117120> Rank : ${index}**
+
+**${Trades.amount} ${Trades.blockchain} (${Trades.amount_usd} USD) ${Trades.transaction_type} From ${FO} Wallet To ${TO}**
+`)
+            EMBED.setTimestamp()
+            client.channels.cache.get(Channels.Trades).send({ embeds: [EMBED] })
+
+        })
+
     }).catch(error => {
-        console.log(error);
+        client.channels.cache.get(Channels.ERR).send('```log\n' + error + '\n```')
     });
 }
