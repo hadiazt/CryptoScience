e8135ed0e308c03f485fa3c1bca26d93b62f8c4c
unknown <hadiazari30@gmail.com>
2022-07-25 14:18:31 +0430

    Update WhaleTrades.Module.js

--- src/Functions/Ready/WhaleTrades.Module.js
+++ src/Functions/Ready/WhaleTrades.Module.js
@@ -26,7 +26,7 @@ module.exports = (client) => {
             EMBED.setThumbnail(client.user.displayAvatarURL({ size: 2048 }))
             EMBED.setDescription(`
 
-**<:medal:998614813578117120> Rank : ${index}**
+**<:medal:998614813578117120> Rank : ${index + 1}**
 
 **${Trades.amount} ${Trades.blockchain} (${Trades.amount_usd} USD) ${Trades.transaction_type} From ${FO} Wallet To ${TO}**
 `)
