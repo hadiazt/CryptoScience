d88eddd4f43ff531a18494c3d32207b72837a258
hadi <65118892+hadiazt@users.noreply.github.com>
2022-08-28 10:50:28 +0430

    Update APIEndPoints.js

--- data/APIEndPoints.js
+++ data/APIEndPoints.js
@@ -1,4 +1,4 @@
-const { APIKEY } = process.env.APIKEY
+const APIKEY = process.env.APIKEY
 
 exports.BaseURL = "https://api.coinstats.app/public/v1";
 exports.BaseURL2 = "https://api.whale-alert.io/v1";
